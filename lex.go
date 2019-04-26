ac45860ebb024e3cebe3d65eaa7fcc0ce74c22c6
Niklas Fasching <niklas.fasching@gmail.com>
2019-04-26 22:22:27 +0200

    Simplify adding custom matchers
    Rather than exposing MatchChars we can also just look into Matchers to check
    whether a function is defined - saves us a variable keeping Matchers and
    MatchChars in sync

--- lex.go
+++ lex.go
@@ -37,8 +37,6 @@ const (
 
 const eof = -1
 
-var MatchChars = "~|^$*"
-
 type stateFn func(*lexer) stateFn
 
 type lexer struct {
@@ -164,7 +162,7 @@ func isNameChar(r rune) bool {
 
 func isWhitespace(r rune) bool     { return strings.ContainsRune(" \t\f\r\n", r) }
 func isDigit(r rune) bool          { return '0' <= r && r <= '9' }
-func isMatchChar(r rune) bool      { return strings.ContainsRune(MatchChars, r) }
+func isMatchChar(r rune) bool      { return Matchers[string(r)+"="] != nil }
 func isCombinatorChar(r rune) bool { return strings.ContainsRune("+~>,", r) }
 
 func acceptIdentifier(l *lexer) error {
